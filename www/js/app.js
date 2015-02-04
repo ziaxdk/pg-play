8cd7ffee8397ed5830cf99c0ea09c7fe36cc5440
ziaxdk <ziaxdk@gmail>
2015-02-04 21:36:59 +0100

    ngCordova

--- www/js/app.js
+++ www/js/app.js
@@ -34,6 +34,7 @@ angular.module('starter', ['ionic', 'ngCordova'])
     // to interact with the app.
     $cordovaKeyboard.hideAccessoryBar(true);
     $cordovaStatusbar.overlaysWebView(true);
+    alert($cordovaStatusbar.styleBlackTranslucent);
     $cordovaStatusbar.styleBlackTranslucent();
   });
 });
